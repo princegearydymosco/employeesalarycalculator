997d6d41e65c261cc071a21ca7efd93f1d62ed9d
princegearydymosco <princegearydymosco@gmail.com>
2023-04-19 00:50:49 +0800

    first commit

---
+++ EmployeeSalaryCalculator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+public abstract class Employee
+{
+    public string Id { get; private set; }
+    public double HoursWorked { get; set; }
+    public double TotalSalary { get; protected set; }
+
+    public abstract void CalculateSalary();
+
+    public void PrintSalary()
+    {
+        Console.WriteLine($"Employee ID: {Id}");
+        Console.WriteLine($"Hours Worked: {HoursWorked}");
+        Console.WriteLine($"Total Salary: PHP {TotalSalary}");
+    }
+}
+
+public class PermanentEmployee : Employee
+{
+    public override void CalculateSalary()
+    {
+        double hourlyRate = 750;
+        double bonus = HoursWorked > 200 ? 750 : 0;
+
+        TotalSalary = HoursWorked * hourlyRate + bonus;
+    }
+}
+
+public class HourlyEmployee : Employee
+{
+    public override void CalculateSalary()
+    {
+        double hourlyRate = 1000;
+        double bonus = HoursWorked > 250 ? 500 : 0;
+
+        TotalSalary = HoursWorked * hourlyRate + bonus;
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        string id = "";
+
+        while (id.Length == 0)
+        {
+            Console.Write("Enter employee ID: ");
+            id = (Console.ReadLine() ?? "").Trim().ToUpper();
+
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                Console.WriteLine("Invalid input. Please enter a non-empty employee ID.");
+            }
+            else if (id[0] != 'P' && id[0] != 'H')
+            {
+                Console.WriteLine("Invalid input. Employee ID must start with 'P' or 'H'.");
+            }
+            else
+            {
+                break;
+            }
+
+            id = "";
+        }
+
+        Employee employee;
+        double hoursWorked;
+
+        if (id.StartsWith("P"))
+        {
+            employee = new PermanentEmployee();
+        }
+        else
+        {
+            employee = new HourlyEmployee();
+        }
+
+        Console.Write("Enter hours worked: ");
+        while (!double.TryParse(Console.ReadLine(), out hoursWorked))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+            Console.Write("Enter hours worked: ");
+        }
+
+        employee.HoursWorked = hoursWorked;
+        employee.CalculateSalary();
+        employee.PrintSalary();
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Console Application for Calculating Employee Salary
+
+This console application in C# calculates the salary of a permanent or hourly employee based on the hours worked. The program prompts the user to enter an employee ID and the number of hours worked. It then calculates the total salary of the employee and displays the result on the console.
+
+## Installation
+
+To use this console application, you need to have Visual Studio installed on your machine. Once you have Visual Studio installed, follow these steps:
+
+1. Clone the repository to your local machine.
+git clone https://github.com/princegearydymosco/employeesalarycalculator.git
+2. Open the solution file in Visual Studio.
+3. Build the solution.
+
+## Usage
+
+To use this console application, follow these steps:
+
+1. Run the console application by pressing F5 in Visual Studio or by navigating to the executable file in the command prompt.
+2. Enter a valid employee ID (must start with 'P' or 'H') and the number of hours worked.
+3. The program will calculate the total salary and display the result on the console.
